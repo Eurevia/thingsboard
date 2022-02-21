bf934011f3d03be1a3ca04fe1b20ce89f0ac3f96
Yannick Simon <yannick.simon@gmail.com>
2022-02-21 18:42:07 +0100

    compilation.sh

--- compilation.sh
+++ compilation.sh
@@ -1,3 +1,6 @@
 #cf https://thingsboard.io/docs/user-guide/install/building-from-source/
-git checkout release-3.3
-mvn clean install -Dlicense.skip=true -DskipTests -Dlicense.skip
+# git checkout release-3.3
+
+rm -Rf './ui-ngx/node_modules'
+export NODE_OPTIONS="--max-old-space-size=8192"
+mvn clean install -DskipTests -Dlicense.skip
